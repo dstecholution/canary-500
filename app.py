82b5aa2283cb1dae2e7b79bc8c469dda23dee5e4
Dwight Spencer <dwight.spencer@techolution.com>
2023-04-03 16:15:58 +0000

    (initial commit)

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Test Thrown", 500
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 3000), debug=True)
